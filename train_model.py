dfb72678cf5081118759d9d6e843c61fde1d1337
jakeboydston12 <jake.boydston@gmail.com>
2026-02-02 19:25:33 -0800

    working on path errors

--- train_model.py
+++ train_model.py
@@ -14,8 +14,8 @@ from ml.model import (
 )
 # TODO: load the cencus.csv data
 
-project_path = os.getcwd()  
-data_path = "/home/jboyds8/Deploying-a-Scalable-ML-Pipeline-with-FastAPI/data/census.csv"
+project_path = os.path.dirname(os.path.abspath(__file__)) 
+data_path = os.path.join(project_path, "data", "census.csv")
 
 data = pd.read_csv(data_path, skipinitialspace=True)
 
